661156e12bd774beb769e5613795f8f21fac88fb
Ian <ian.nasoore@student.moringaschool.com>
2025-10-28 14:36:35 +0300

    edu

---
+++ src/components/Article.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  function getReadTime(minutes) {
+    if (!minutes) return null;
+
+    if (minutes < 30) {
+      const cups = Math.ceil(minutes / 5);
+      return "☕️".repeat(cups) + ` ${minutes} min read`;
+    } else {
+      const bentos = Math.ceil(minutes / 10);
+      return "🍱".repeat(bentos) + ` ${minutes} min read`;
+    }
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date}
+        {minutes && " • "}
+        {getReadTime(minutes)}
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
